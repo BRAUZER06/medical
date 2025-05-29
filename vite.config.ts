347aeec9c633dd5cbe51aadeeaa2748364e0cd69
potatofreedom <gustovsson6@gmail.com>
2025-05-29 14:23:54 +0700

    rollback

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    allowedHosts: ['only-doc.ru'],
-    proxy: {
-      '/api': {
-        target: 'https://only-doc.ru/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
+  plugins: [react()],
+})
+
