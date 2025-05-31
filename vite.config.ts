f84610e2209ed100204c1f706eebecc10e2aeb03
potatofreedom <gustovsson6@gmail.com>
2025-06-01 00:33:28 +0700

    added access work directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   root: '.',
   plugins: [react()],
@@ -13,6 +12,9 @@ export default defineConfig({
     hmr: {
       clientPort: 443,
       port: 5173
+    },
+    fs: {
+      allow: ['.']
     }
   },
 })
